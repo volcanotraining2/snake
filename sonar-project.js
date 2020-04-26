3144d685afe429fb78e28998ff1fe7756edebae0
volcanotraining2 <64292630+volcanotraining2@users.noreply.github.com>
2020-04-26 11:09:22 +1000

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://192.168.56.101:9000',
+       serverUrl: 'http://35.197.170.186:9000',
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
